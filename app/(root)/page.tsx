25366ea2b3e36ceffff4523e1c895e23f29caf4c
Moksh Jhaveri <moksh1027@gmail.com>
2025-10-02 18:55:55 +0530

    changes made for the ui now will integrate firebase

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,31 @@
 import react from 'react';
-
+import Link from 'next/link';
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
 const Home = ()=>{
   return(
-    <div>Home</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+          <p className="text-lg">
+            Practice real interview questions & get instant feedback
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image
+          src="/robot.png"
+          alt="robo-dude"
+          width={400}
+          height={400}
+          className="max-sm:hidden"
+        />
+      </section>
+    </>
   )
 }
 
